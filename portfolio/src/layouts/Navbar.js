ff476f55eab6f5f22c33a0237811b4f9ed0c6cb8
Ally4 <el.ally741@gmail.com>
2020-07-16 23:35:33 +0200

    home-page: landing-page-of-the-portfolio

---
+++ portfolio/src/layouts/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+        <div className="navbar">
+            <div ><h2>Ally</h2></div>
+            <div class="btn" id="buttons"><a href="#">Home</a></div>
+            <div class="btn" id="buttons"><a href="#">About</a></div>
+            <div class="btn" id="buttons"><a href="#">Education</a></div>
+            <div class="btn" id="buttons"><a href="#">Resume</a></div>
+            <div class="btn" id="buttons"><a href="#">Blog</a></div>
+            <div class="btn" id="buttons"><a href="#">Contact</a></div>
+        </div>
+        )
+    }
+}
+
+export default Navbar;
