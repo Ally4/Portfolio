69fbf8c17ac856a653dfbfff9fa654121645aa62
Ally4 <el.ally741@gmail.com>
2020-07-21 05:18:35 +0200

    feature(landing-page):home-page feedbacks-and-updates

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -4,9 +4,7 @@ class Navbar extends Component {
     render() {
         return (
         <div className="navbar">
-            <div ><h2>Ally</h2></div>
             <div class="btn" id="buttons"><a href="#"  style={{color:"#F3C00A"}}>Home</a></div>
-            <div class="btn" id="buttons"><a href="#">About</a></div>
             <div class="btn" id="buttons"><a href="#">Education</a></div>
             <div class="btn" id="buttons"><a href="#">Resume</a></div>
             <div class="btn" id="buttons"><a href="#">Blog</a></div>
