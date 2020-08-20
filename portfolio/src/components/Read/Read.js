01f76060a2ac2016ec3f0f02cc6299dd9d5b5771
Ally4 <el.ally741@gmail.com>
2020-08-20 12:07:03 +0200

    feature(Feedback):User Interface Deletion

--- portfolio/src/components/Read/Read.js
+++ portfolio/src/components/Read/Read.js
@@ -6,7 +6,6 @@ import "./Read.css";
 const db = fire.firestore();
 
 const Read = (blog) => {
-console.log(`bbbbbbbbbbb${blog.title}`);
   return (
     <div className="action">
       <button className="edit" value="Edit"><Link to={`/blog/read/${blog.id}`}>Read More</Link>       
